775d83c5f12d3ead494e8fdb1f5613947ec93e57
SenneDW <71550095+SenneDW@users.noreply.github.com>
2023-10-12 10:40:24 +0200

    Update App.js

--- spotify/src/App.js
+++ spotify/src/App.js
@@ -24,7 +24,7 @@ export const App = () => {
       </video>
       <div className='landingpageText'>
         <h1 className='landingpageTitle'>Explore your Spotify stats - Sign in for music magic!</h1>
-        <button className='landingpageButton' onClick={() => window.location.href = "https://accounts.spotify.com/authorize?client_id=9425ae1253be4d83a33505446261e2f9&response_type=code&redirect_uri=http://localhost:3000/account&scope=user-top-read%20user-read-private&show_dialog=true"} >Sign in with Spotify <img className='buttonImage' src={spotifyLogo}></img> </button>
+        <button className='landingpageButton' onClick={() => window.location.href = "https://accounts.spotify.com/authorize?client_id=xxxxxxx&response_type=code&redirect_uri=http://localhost:3000/account&scope=user-top-read%20user-read-private&show_dialog=true"} >Sign in with Spotify <img className='buttonImage' src={spotifyLogo}></img> </button>
       </div>
     </div>
   );
